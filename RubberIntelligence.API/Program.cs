3239781cd92a6bd9fb5f2967bcc5730215f78c05
IT22355928 <127284982+IT22355928@users.noreply.github.com>
2026-01-26 11:47:15 +0530

    update backend files

--- RubberIntelligence.API/Program.cs
+++ RubberIntelligence.API/Program.cs
@@ -93,6 +93,16 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         };
     });
 
+// Add CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder => builder
+        .AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader());
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -103,6 +113,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
 
 app.UseAuthentication();
 app.UseAuthorization();
